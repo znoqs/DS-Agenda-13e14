c6922611ab4a1c707806f5891d246b536df94b36
Enzo Sibuta <sibutaenzo@gmail.com>
2024-11-09 22:12:08 -0300

    Alterações página hospedagem contratada

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -6,4 +6,17 @@ public partial class ContratacaoHospedagem : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			
+			Navigation.PushAsync(new HospedagemContratada());
+
+		}catch (Exception ex) 
+		{
+			DisplayAlert("Ops", ex.Message, "Ok");
+		}
+    }
 }
